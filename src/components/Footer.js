33239ed4d2e125b4a65d171cb458a605af75656b
mateuszurban <65718811+mateuszurban@users.noreply.github.com>
2023-06-14 19:24:11 +0200

    Add navbar styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,10 @@ const Footer = () => {
                 <li>FAQ</li>
                 <li>contact</li>
             </ul>
+            <ul>
+                <p>Credits:</p>
+            <a href="https://www.flaticon.com/free-icons/coffee-bean" title="coffee bean icons">Coffee bean icons created by photo3idea_studio - Flaticon</a>
+            </ul>
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,14 @@ import React from 'react'
 const Nav = () => {
     return (
         <div className="navbar">
-            <ul>
-            <li>Apollo Cafe</li>
-            <li>Mission</li>
-            <li>Team</li>
-            <li>Stories</li>
-            <li>Store</li>
-        </ul>
+            <ul>               
+                <div className="logo"></div>
+                <li>Apollo Cafe</li>
+                <li>Mission</li>
+                <li>Team</li>
+                <li>Stories</li>
+                <li>Store</li>
+            </ul>
         </div>
     )
 }
