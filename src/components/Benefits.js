a56fc1d8b6d6abc88178999d30801afdb48d8a1c
mateuszurban <65718811+mateuszurban@users.noreply.github.com>
2023-06-22 17:16:21 +0200

    Add 'benefits' section

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -3,10 +3,12 @@ import React from 'react'
 const Benefits = () => {
     return (
         <div className="benefits">
-            <h2>Consectetur adipisicing elit.</h2>
-            <p> Excepturi temporibus, ea deserunt nostrum eligendi tempora nemo molestias nobis dignissimos odit quo, nam voluptatibus ut porro recusandae quidem eveniet? Sit doloribus accusantium voluptas vero ea, aut fugit facilis delectus ab illum eligendi cum ipsa numquam inventore! </p>
-            <img src="" alt="" />
-            {/* relevant picture, vertically mirrors the one above */}
+            <div className="benefits-img"></div>
+            <div className="benefits-headline">
+                <h2>Company that serves.</h2>
+                <p>Apollo Cafe always aims towards fair trade practices. Income stability for farmers, promoting healthy environment and empowering communities. For now <em>and</em> for the future.</p>
+                <button className='button'>Visit our farms</button>
+            </div>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,6 +14,8 @@ const Footer = () => {
             <a href="https://www.flaticon.com/free-icons/coffee-bean" title="coffee bean icons">Coffee bean icons created by photo3idea_studio - Flaticon</a>
             <br />
             Photo by <a href="https://unsplash.com/@devintavery?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Devin Avery</a> on <a href="https://unsplash.com/photos/kAV1KsIiqgw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            
+            <br />Photo by <a href="https://unsplash.com/@katya?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Katya Ross</a> on <a href="https://unsplash.com/photos/Adg9B8NTxj0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
             </ul>
         </div>
     )
